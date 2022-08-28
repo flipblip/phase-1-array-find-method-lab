1eaebf1ff9ae893ed8f51bb300fe84687d79df0c
fliblip <wickynjy@gmail.com>
2022-08-28 18:31:48 +0300

    Weuh.First test passing

--- index.js
+++ index.js
@@ -1,1 +1,40 @@
-// code your solution here
+const record = [
+    { year: "2015", result: "W"},
+    { year: "2014", result: "N/A"},
+    { year: "2013", result: "L"},
+    //...
+  ]
+
+
+
+
+function superbowlWin(Array){
+    const found = Array.find(({result}) => result  === "W");
+    
+    if (typeof(found) === 'object'){
+        console.log(found);
+    }
+
+    // return result;
+}
+
+superbowlWin(record)
+
+
+// function logSuperBowlWin(Array, search){
+//     let result 
+//     let found = Array.find(element => element(''))
+
+//     result.push(found)
+// }
+
+// function search(record){
+//     return  record.result === 'W';
+// }
+
+
+// function superbowlWin(Array){
+//     return record.result = 'W';
+// }
+
+// console.log(record.find(superbowlWin))
